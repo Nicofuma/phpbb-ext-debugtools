ead6418cb1ae333847d1888439da477d47eaf015
Tristan Darricau <github@nicofuma.fr>
2014-10-22 10:44:54 +0200

    Fix headers files

--- console/command/twig/lint.php
+++ console/command/twig/lint.php
@@ -1,14 +1,11 @@
 <?php
 /**
  *
- * This file is part of the phpBB Forum Software package.
+ * Debug Tools extension for the phpBB Forum Software package.
  *
  * @copyright (c) phpBB Limited <https://www.phpbb.com>
  * @copyright (c) Fabien Potencier <fabien@symfony.com>
- * @license       GNU General Public License, version 2 (GPL-2.0)
- *
- * For full copyright and license information, please see
- * the docs/CREDITS.txt file.
+ * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
 
